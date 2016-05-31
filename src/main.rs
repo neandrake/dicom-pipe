dde8649e8b064b13d5c2f0e61e7e3b1edfd5f79a
Christopher Speck <die.drachen@gmail.com>
2016-05-30 20:37:47 -0400

    Ran rustfmt

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate walkdir;
 
 mod read;
 
-use read::DicomStream; 
+use read::DicomStream;
 
 use std::fs::File;
 use std::io::{Error, Write};
@@ -16,42 +16,38 @@ static FIXTURE_DATASET1_FOLDER: &'static str = "fixtures/dataset1";
 impl DicomStream for File {}
 
 fn main() {
-	let dirwalker: WalkDir = WalkDir::new(FIXTURE_DATASET1_FOLDER)
-										.min_depth(1)
-										.max_depth(1);
-	match parse_directory(dirwalker) {
-		Ok(()) => {
-			
-		},
-		Err(err) => {
-			writeln!(
-				&mut std::io::stderr(),
-				"[ERROR] Error accessing directory: {:?}",
-				err
-			).unwrap();
-		}
-	}
+    let dirwalker: WalkDir = WalkDir::new(FIXTURE_DATASET1_FOLDER)
+        .min_depth(1)
+        .max_depth(1);
+    match parse_directory(dirwalker) {
+        Ok(()) => {}
+        Err(err) => {
+            writeln!(&mut std::io::stderr(),
+                     "[ERROR] Error accessing directory: {:?}",
+                     err)
+                .unwrap();
+        }
+    }
 }
 
 fn parse_directory(dirwalker: WalkDir) -> Result<(), Error> {
-	let dirwalker_iter: walkdir::Iter = dirwalker.into_iter();
-	let dir_entries = dirwalker_iter.filter_entry(|e| !read::is_hidden(e.path()));
-	for entry_res in dir_entries {
-		let entry: DirEntry = try!(entry_res);
-		let path: &Path = entry.path();
-		let dstream_res: Result<Box<DicomStream>, Error> = read::open_file_as_dicom_stream(&path);
-		match dstream_res {
-			Ok(_) => {
-				println!("[DEBUG] File is DICOM: {:?}", path);
-			},
-			Err(err) => {
-				writeln!(
-					&mut std::io::stderr(),
-					"[ERROR] Error parsing file as DICOM: {:?}",
-					err
-				).unwrap();
-			}
-		}
-	}
-	return Result::Ok(());
+    let dirwalker_iter: walkdir::Iter = dirwalker.into_iter();
+    let dir_entries = dirwalker_iter.filter_entry(|e| !read::is_hidden(e.path()));
+    for entry_res in dir_entries {
+        let entry: DirEntry = try!(entry_res);
+        let path: &Path = entry.path();
+        let dstream_res: Result<Box<DicomStream>, Error> = read::open_file_as_dicom_stream(&path);
+        match dstream_res {
+            Ok(_) => {
+                println!("[DEBUG] File is DICOM: {:?}", path);
+            }
+            Err(err) => {
+                writeln!(&mut std::io::stderr(),
+                         "[ERROR] Error parsing file as DICOM: {:?}",
+                         err)
+                    .unwrap();
+            }
+        }
+    }
+    return Result::Ok(());
 }
--- src/read/mod.rs
+++ src/read/mod.rs
@@ -5,56 +5,57 @@ use std::fs::File;
 use std::io::{Error, ErrorKind, Read, Seek, SeekFrom};
 use std::path::Path;
 
-pub trait DicomStream : Read + Seek {
+pub trait DicomStream: Read + Seek {
     fn is_standard_dicom(&mut self) -> Result<bool, Error> {
-        let filler_size : usize = 128;
-        let preamble_size : usize = 4;
-        let buf_size : usize = filler_size + preamble_size;
-        
+        let filler_size: usize = 128;
+        let preamble_size: usize = 4;
+        let buf_size: usize = filler_size + preamble_size;
+
         // mark the current position, seek to beginning of stream to read preamble, seek back to current position
-        let start_pos : u64 = try!(self.seek(SeekFrom::Current(0)));
+        let start_pos: u64 = try!(self.seek(SeekFrom::Current(0)));
         try!(self.seek(SeekFrom::Start(0)));
-        let mut buffer : Vec<u8> = vec![0;buf_size];
+        let mut buffer: Vec<u8> = vec![0;buf_size];
         try!(self.read_exact(buffer.as_mut_slice()));
         try!(self.seek(SeekFrom::Start(start_pos)));
-        
+
         // check that first 128 bytes are 0, followed by 'D', 'I', 'C', 'M'
-        
+
         for n in 0..(filler_size) {
             if buffer[n] != 0 {
                 return Result::Ok(false);
             }
         }
-        
-        let preamble : Vec<u8> = vec!['D' as u8, 'I' as u8, 'C' as u8, 'M' as u8];
-        let slice : &[u8] = &buffer[filler_size..filler_size + preamble_size];
+
+        let preamble: Vec<u8> = vec!['D' as u8, 'I' as u8, 'C' as u8, 'M' as u8];
+        let slice: &[u8] = &buffer[filler_size..filler_size + preamble_size];
         for n in 0..preamble_size {
             if slice[n] != preamble[n] {
                 return Result::Ok(false);
             }
         }
-        
+
         Result::Ok(true)
     }
 }
 
 
 pub fn open_file_as_dicom_stream(path: &Path) -> Result<Box<DicomStream>, Error> {
-	if !path.is_file() {
-		return Result::Err(Error::new(ErrorKind::InvalidData, format!("Invalid file: {:?}", path)));
-	}
+    if !path.is_file() {
+        return Result::Err(Error::new(ErrorKind::InvalidData, format!("Invalid file: {:?}", path)));
+    }
 
-	let mut fstream: File = try!(File::open(path));
-	let is_dcm: bool = try!(fstream.is_standard_dicom());
-	if is_dcm {
-		return Result::Ok(Box::new(fstream));
-	}
-	return Result::Err(Error::new(ErrorKind::InvalidData, format!("File is not DICOM: {:?}", path)));
+    let mut fstream: File = try!(File::open(path));
+    let is_dcm: bool = try!(fstream.is_standard_dicom());
+    if is_dcm {
+        return Result::Ok(Box::new(fstream));
+    }
+    return Result::Err(Error::new(ErrorKind::InvalidData,
+                                  format!("File is not DICOM: {:?}", path)));
 }
 
 pub fn is_hidden(path: &Path) -> bool {
     path.file_name()
-         .map(|s| s.to_str().unwrap_or(""))
-         .map(|s| s.starts_with("."))
-         .unwrap_or(false)
+        .map(|s| s.to_str().unwrap_or(""))
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
 }
