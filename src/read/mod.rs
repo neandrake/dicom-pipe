bfde8ba317b169c0cffc1c022e6eda21909f0567
cspeck <cspeck@localhost>
2016-06-15 00:25:56 -0400

    Updates to reading -- DICOM File Meta Information is being read appropriately (required fields only)

--- src/read/mod.rs
+++ src/read/mod.rs
@@ -8,33 +8,25 @@ mod mock;
 
 use byteorder::{ByteOrder, LittleEndian, ReadBytesExt};
 
+use std::fmt;
 use std::fs::File;
-use std::io::{Error, ErrorKind, Read, Seek, SeekFrom};
+use std::io::{Error, ErrorKind, Read, Seek};
 use std::path::Path;
 
 use util::vr::VR;
 
 
-const FILE_PREAMBLE_SIZE: usize = 128;
-static DICOM_PREFIX: [u8; 4] = ['D' as u8, 'I' as u8, 'C' as u8, 'M' as u8];
+const FILE_PREAMBLE_LENGTH: usize = 128;
+const DICOM_PREFIX_LENGTH: usize = 4;
+static DICOM_PREFIX: [u8;DICOM_PREFIX_LENGTH] = ['D' as u8, 'I' as u8, 'C' as u8, 'M' as u8];
 
 
 pub struct DicomStream<StreamType> {
     stream: StreamType,
 
-    file_preamble: [u8;128],
-    dicom_prefix: [u8;4],
+    file_preamble: [u8;FILE_PREAMBLE_LENGTH],
+    dicom_prefix: [u8;DICOM_PREFIX_LENGTH],
     
-    /*
-    file_meta_info_group_length: DicomElement,
-    file_meta_info_version: DicomElement,
-    
-    media_storage_sop_class_uid: DicomElement,
-    media_storage_sop_instance_uid: DicomElement,
-    transfer_syntax_uid: DicomElement,
-    implementation_class_uid: DicomElement,
-    */
-
     dicom_header: Vec<DicomElement>,
 }
 
@@ -45,6 +37,12 @@ pub struct DicomElement {
     pub bytes: Vec<u8>,
 }
 
+impl fmt::Debug for DicomElement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "DicomElement {{ tag: 0x{:08X}  vr: {}  vl: {} }}", self.tag, self.vr.ident, self.vl)
+    }
+}
+
 impl DicomStream<File> {
     pub fn new_from_path(path: &Path) -> Result<DicomStream<File>, Error> {
         if !path.is_file() {
@@ -52,13 +50,8 @@ impl DicomStream<File> {
                                           format!("Invalid path: {:?}", path)));
         }
 
-        let mut fstream: DicomStream<File> = DicomStream::new(try!(File::open(path)));
-        let is_dcm: bool = try!(fstream.is_standard_dicom());
-        if is_dcm {
-            return Result::Ok(fstream);
-        }
-        return Result::Err(Error::new(ErrorKind::InvalidData,
-                                      format!("File is not DICOM: {:?}", path)));
+        let file: File = try!(File::open(path));
+        Result::Ok::<DicomStream<File>, Error>(DicomStream::new(file))
     }
 }
 
@@ -66,8 +59,8 @@ impl<StreamType: ReadBytesExt + Seek> DicomStream<StreamType> {
     pub fn new(stream: StreamType) -> DicomStream<StreamType> {
         DicomStream {
             stream: stream,
-            file_preamble: [0u8;128],
-            dicom_prefix: [0u8;4],
+            file_preamble: [0u8;FILE_PREAMBLE_LENGTH],
+            dicom_prefix: [0u8;DICOM_PREFIX_LENGTH],
             dicom_header: Vec::with_capacity(6),
         }
     }
@@ -78,12 +71,12 @@ impl<StreamType: ReadBytesExt + Seek> DicomStream<StreamType> {
     }
 
     #[allow(dead_code)]
-    pub fn get_file_preamble(&self) -> &[u8;128] {
+    pub fn get_file_preamble(&self) -> &[u8;FILE_PREAMBLE_LENGTH] {
         &self.file_preamble
     }
 
     #[allow(dead_code)]
-    pub fn get_dicom_prefix(&self) -> &[u8;4] {
+    pub fn get_dicom_prefix(&self) -> &[u8;DICOM_PREFIX_LENGTH] {
         &self.dicom_prefix
     }
 
@@ -95,7 +88,6 @@ impl<StreamType: ReadBytesExt + Seek> DicomStream<StreamType> {
     pub fn read_dicom_prefix(&mut self) -> Result<(), Error> {
         try!(self.stream.read_exact(&mut self.dicom_prefix));
 
-        // check that first 128 bytes are 0, followed by 'D', 'I', 'C', 'M'
         for n in 0..DICOM_PREFIX.len() {
             if self.dicom_prefix[n] != DICOM_PREFIX[n] {
                 return Result::Err(Error::new(ErrorKind::InvalidData,
@@ -110,7 +102,6 @@ impl<StreamType: ReadBytesExt + Seek> DicomStream<StreamType> {
         let first: u32 = (try!(self.stream.read_u16::<LittleEndian>()) as u32) << 16;
         let second: u32 = try!(self.stream.read_u16::<LittleEndian>()) as u32;
         let result: u32 = first + second;
-        println!("READ TAG: {:?}", result);
         Result::Ok(result)
     }
 
@@ -118,7 +109,6 @@ impl<StreamType: ReadBytesExt + Seek> DicomStream<StreamType> {
         let first: u8 = try!(self.stream.read_u8());
         let second: u8 = try!(self.stream.read_u8());
         let code: u16 = ((first as u16) << 8) + second as u16;
-        println!("READ VR: {:?}", code);
         VR::code_to_vr(code).ok_or(Error::new(ErrorKind::InvalidData, format!("Unable to interpret VR: {:?}", code)))
     }
 
@@ -126,9 +116,8 @@ impl<StreamType: ReadBytesExt + Seek> DicomStream<StreamType> {
         match vr.explicit_vr_header_length {
             8 => self.stream.read_u16::<LittleEndian>().map(|n| n as u32),
             12 => {
-                let first: u32 = (try!(self.stream.read_u16::<LittleEndian>()) as u32) << 16;
-                let second: u32 = try!(self.stream.read_u16::<LittleEndian>()) as u32;
-                Result::Ok(first + second)
+                try!(self.stream.read_u16::<LittleEndian>());
+                self.stream.read_u32::<LittleEndian>()
             },
             n => Err(Error::new(ErrorKind::InvalidData, format!("Invalid VR Header Length: {:?}", n))),
         }
@@ -137,7 +126,6 @@ impl<StreamType: ReadBytesExt + Seek> DicomStream<StreamType> {
     pub fn read_value_field(&mut self, value_length: u32) -> Result<Vec<u8>, Error> {
         let mut bytes: Vec<u8> = vec![0;value_length as usize];
         try!(self.stream.read_exact(bytes.as_mut_slice()));
-        println!("READ VALUE LENGTH: {:?}", value_length);
         Result::Ok(bytes)
     }
 
@@ -160,11 +148,22 @@ impl<StreamType: ReadBytesExt + Seek> DicomStream<StreamType> {
         try!(self.read_dicom_prefix());
 
         let file_meta_info_group_length: DicomElement = try!(self.read_dicom_element());
+        println!("File Meta Information Group Length: {:?}", file_meta_info_group_length);
+
         let file_meta_info_version: DicomElement = try!(self.read_dicom_element());
+        println!("File Meta Information Version: {:?}", file_meta_info_version);
+
         let media_storage_sop_class_uid: DicomElement = try!(self.read_dicom_element());
+        println!("Media Storage SOP Class UID: {:?}", media_storage_sop_class_uid);
+
         let media_storage_sop_instance_uid: DicomElement = try!(self.read_dicom_element());
+        println!("Media Storage SOP Instance UID: {:?}", media_storage_sop_instance_uid);
+
         let transfer_syntax_uid: DicomElement = try!(self.read_dicom_element());
+        println!("Transfer Syntax UID: {:?}", transfer_syntax_uid);
+
         let implementation_class_uid: DicomElement = try!(self.read_dicom_element());
+        println!("Implementation Class UID: {:?}", implementation_class_uid);
 
         self.dicom_header.push(file_meta_info_group_length);
         self.dicom_header.push(file_meta_info_version);
@@ -174,32 +173,5 @@ impl<StreamType: ReadBytesExt + Seek> DicomStream<StreamType> {
         self.dicom_header.push(implementation_class_uid);
 
         Result::Ok(())
-    }
-
-    pub fn is_standard_dicom(&mut self) -> Result<bool, Error> {
-        let buf_size: usize = FILE_PREAMBLE_SIZE + DICOM_PREFIX.len();
-
-        // mark the current position, seek to beginning of stream to read preamble, seek back to current position
-        let start_pos: u64 = try!(self.stream.seek(SeekFrom::Current(0)));
-        try!(self.stream.seek(SeekFrom::Start(0)));
-        let mut buffer: Vec<u8> = vec![0;buf_size];
-        try!(self.stream.read_exact(buffer.as_mut_slice()));
-        try!(self.stream.seek(SeekFrom::Start(start_pos)));
-
-        // check that first 128 bytes are 0, followed by 'D', 'I', 'C', 'M'
-        for n in 0..FILE_PREAMBLE_SIZE {
-            if buffer[n] != 0 {
-                return Result::Ok(false);
-            }
-        }
-
-        let slice: &[u8] = &buffer[FILE_PREAMBLE_SIZE..FILE_PREAMBLE_SIZE + DICOM_PREFIX.len()];
-        for n in 0..DICOM_PREFIX.len() {
-            if slice[n] != DICOM_PREFIX[n] {
-                return Result::Ok(false);
-            }
-        }
-
-        Result::Ok(true)
     }
 }
