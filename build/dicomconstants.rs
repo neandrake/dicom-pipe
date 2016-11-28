dbcb5bc4df8f5c0d6e0e262b99d17590da43d092
cspeck <cspeck@localhost>
2016-11-27 22:37:34 -0500

    Task 2, Task 3 - Putting in place the automation of parsing DICOM elements/UIDs into rust code 1. Added a separate `src/core` module for the new struct definitions and generated definitions to exist 2. Created new struct definitions which are mostly copy/pasted from existing for the moment 3. Utilizing the `[package.build]` property in `Cargo.toml` to have a custom build script that parses the HTML files to rust definitions 4. Implemented a large part of the build script however it does not currently generate compilable rust script.
    Caveats with this current approach:
    1. The build script will run when it recognizes any of the HTML files have been newly modified, however the intent of the build script is to write out new rust code into `OUT_DIR` and be compiled with. As for this purpose I would like these definitions to be available in the repository and usable for intelli-sense the build script is manually placing the files into the repository instead. This means however that since it doesn't write out rust files into `OUT_DIR` they will not be compiled as part of the current compliation -> if the DICOM definitions update it requires building twice.
    2. I would prefer this to exist as a separate build task of some sort (a la `make` or `gradle`) as there can only ever be a single custom build script per crate. The benefit to this current approach is that most of the code should be usable if this gets moved into a separate toolchain.
    
    Refs T2
    Refs T3

---
+++ build/dicomconstants.rs
@@ -0,0 +1,276 @@
+extern crate csv;
+extern crate htmltbl2csv;
+extern crate rustc_serialize;
+extern crate walkdir;
+
+use csv::Reader;
+
+use htmltbl2csv::CsvTable;
+
+use std::fs::File;
+use std::io::{Cursor, Error, ErrorKind, Write};
+use std::path::Path;
+
+use walkdir::{DirEntry, WalkDir};
+
+static DICOM_CONSTANTS_HTML_FOLDER: &'static str = "build/dicom_constants_html/";
+
+fn main() {
+    let files: Vec<DirEntry> = collect_files();
+
+    print_rerun_if_changed(&files)
+        .expect("Unable to build rerun-if-changed");
+    
+    process_html_files(&files)
+        .expect("Unable to build code files");
+}
+
+/// Gets the list of all files in the `DICOM_CONSTANTS_HTML_FOLDER` location
+fn collect_files() -> Vec<DirEntry> {
+    let dirwalker: WalkDir = WalkDir::new(DICOM_CONSTANTS_HTML_FOLDER)
+        .min_depth(1)
+        .max_depth(1);
+    
+    let mut files: Vec<DirEntry> = Vec::new();
+    for dir_entry in dirwalker.into_iter() {
+        if let Ok(entry) = dir_entry {
+            files.push(entry);
+        }
+    }
+
+    files
+}
+
+/// In theory this instructs cargo that this build script doesn't need run
+/// unless the files in the `DICOM_CONSTANTS_HTML_FOLDER` directory changed.
+/// This does appear to work, however cargo is also "smart" and will ensure
+/// this build script runs fresh if this script file has changed even if the
+/// files in `DICOM_CONSTANTS_HTML_FOLDER` haven't.
+fn print_rerun_if_changed(files: &[DirEntry]) -> Result<(), Error> {
+    for entry in files {
+        let path: &Path = entry.path();
+        println!("cargo:rerun-if-changed=./{}", path.to_str().unwrap());
+    }
+    Ok(())
+}
+
+/// Processes the HTML files in the given directory into CSV
+/// the converts the CSV format into code, writing files into src/core/
+fn process_html_files(files: &[DirEntry]) -> Result<(), Error> {
+    for entry in files {
+        let path: &Path = entry.path();
+        let path_str: &str = path.to_str().unwrap();
+
+        let table_type: Option<TableType> = TableType::from_filename(path_str);
+        if table_type.is_none() {
+            continue;
+        }
+        let table_type: TableType = table_type.unwrap();
+
+        let csv_tables: Vec<CsvTable> = htmltbl2csv::process_input(path_str)?;
+        let ref csv_table: CsvTable = csv_tables[2];
+        let mut reader: Reader<Cursor<Vec<u8>>> = csv_table.process_to_reader()?;
+
+        let mut defns: String = String::new();
+        if table_type.is_element() {
+            let elements: Vec<DataElement> = reader.decode()
+                .collect::<csv::Result<Vec<DataElement>>>()
+                .map_err(|e: csv::Error| Error::new(ErrorKind::InvalidData, e))?;
+            
+            for element in elements {
+                if let Some(code) = table_type.process_element(&element) {
+                    defns.push_str(&code);
+                }
+            }
+        } else {
+            let uids: Vec<Uid> = reader.decode()
+                .collect::<csv::Result<Vec<Uid>>>()
+                .map_err(|e: csv::Error| Error::new(ErrorKind::InvalidData, e))?;
+
+            for uid in uids {
+                if let Some(code) = table_type.process_uid(&uid) {
+                    defns.push_str(&code);
+                }
+            }
+        }
+
+        let mut out_rs_file: File = File::create(table_type.filename()?)?;
+        let preamble: String = table_type.get_codefile_preamble();
+
+        out_rs_file.write_all(&preamble.into_bytes())?;
+        out_rs_file.write_all(&defns.into_bytes())?;
+        out_rs_file.flush()?;
+        out_rs_file.sync_all()?;
+    }
+
+    Ok(())
+}
+
+/// The different tables we parse out of the HTML/CSV
+#[derive(Eq, PartialEq, Debug)]
+enum TableType {
+    DicomElements,
+    FileMetaElements,
+    DirStructureElements,
+    Uids,
+}
+
+impl TableType {
+    /// Determines the table type being used based on the filename that parsed.
+    /// This is dependent on the filenames following a specific format:
+    /// 
+    /// - "Part6.Ch6" => DicomElements
+    /// - "Part6.Ch7" => FileMetaElements
+    /// - "Part6.Ch8" => DirStructureElements
+    /// - "Part6.ChA" => Uids
+    pub fn from_filename(path_str: &str) -> Option<TableType> {
+        if path_str.contains("Part6.Ch6") { Some(TableType::DicomElements) }
+        else if path_str.contains("Part6.Ch7") { Some(TableType::FileMetaElements) }
+        else if path_str.contains("Part6.Ch8") { Some(TableType::DirStructureElements) }
+        else if path_str.contains("Part6.ChA") { Some(TableType::Uids) }
+        else { None }
+    }
+
+    /// Whether this type is parsing as an element (Tag) or uid (...UID)
+    pub fn is_element(&self) -> bool {
+        match *self {
+            TableType::Uids => false,
+            _ => true
+        }
+    }
+
+    /// The rust output code file that should be written to.
+    /// The path will be relative to the working directory, which for this
+    /// build script is the root of the project
+    pub fn filename(&self) -> Result<&Path, Error> {
+        match *self {
+            TableType::DicomElements => Ok(Path::new("src/core/dicom_elements.rs")),
+            TableType::FileMetaElements => Ok(Path::new("src/core/file_meta_elements.rs")),
+            TableType::DirStructureElements => Ok(Path::new("src/core/dir_structure_elements.rs")),
+            TableType::Uids => Ok(Path::new("src/core/uids.rs")),
+        }
+    }
+
+    /// Cleans up a string to remove/replace characters found in the HTML
+    /// so the variable name will be valid rust
+    /// Some things straight up doesn't have a name aside from "(Retired)"~
+    /// Such as: 1.2.840.10008.5.1.4.1.​1.​40 and (0018,0061)
+    /// So check whether the returned string is empty
+    fn sanitize_var_name(var_name: &String) -> String {
+        var_name
+            .replace(" ", "")
+            .replace("(Retired)", "_Retired")
+            .replace("(Trial)", "_Trial")
+            .replace("-", "")
+            .replace(",", "")
+            .replace("(", "")
+            .replace(")", "")
+            .replace(".", "")
+            .replace("/", "")
+            .replace(":", "_")
+            .replace("&", "_")
+    }
+
+    /// Gets the preamble for the file -- things like `use` statements needed
+    pub fn get_codefile_preamble(&self) -> String {
+        if self.is_element() {
+"
+
+".to_owned()
+        } else {
+"
+".to_owned()
+        }
+    }
+
+    /// Processes a UID entry from CSV and returns the bit of code for it
+    pub fn process_uid(&self, uid: &Uid) -> Option<String> {
+        if self.is_element() {
+            return None;
+        }
+
+        let var_name: String = TableType::sanitize_var_name(&uid.name);
+        if var_name.is_empty() {
+            return None;
+        }
+        
+        let code: String = format!(
+"/// {}
+/// 
+/// - **UID:** {}
+/// - **UID Type:** {}
+pub static {}: UID = UID {{
+    ident: \"{}\",
+    uid: \"{}\",
+    name: \"{}\",
+}};
+
+",
+            // comment
+            uid.name, uid.value, uid.uid_type,
+            // definitions
+            var_name, var_name, uid.value, uid.name);
+
+        Some(code)
+    }
+
+    /// Processes an element entry from CSV and returns the bit of code for it
+    pub fn process_element(&self, element: &DataElement) -> Option<String> {
+        if !self.is_element() {
+            return None;
+        }
+
+        let var_name: String = TableType::sanitize_var_name(&element.keyword);
+        if var_name.is_empty() {
+            return None;
+        }
+
+        let code: String = format!(
+"/// {}
+/// 
+/// - **Tag:** {}
+/// - **VR:** {}
+/// - **VM:** {}
+pub static {}: Tag = Tag {{
+    ident: \"{}\",
+    tag: \"{}\",
+    implicit_vr: Some(&vr::{}),
+    vm: \"{}\",
+    desc: \"{}\",
+}};
+
+",
+            // comment
+            element.name, element.tag, element.vr, element.vm,
+            // definitions
+            var_name, var_name, element.tag, element.vr, element.vm, element.name);
+
+        Some(code)
+    }
+}
+
+// The following decodable structs are used by the CSV reader to
+// convert values from CSV into an struct/object. Each field will map
+// directly to its ordinal column, so order of defined field is important
+
+#[derive(RustcDecodable)]
+struct DataElement {
+    tag: String,
+    name: String,
+    keyword: String,
+    vr: String,
+    vm: String,
+
+    #[allow(dead_code)]
+    misc: String,
+}
+
+#[derive(RustcDecodable)]
+struct Uid {
+    value: String,
+    name: String,
+    uid_type: String,
+
+    #[allow(dead_code)]
+    part: String,
+}
---
+++ src/core/dir_structure_elements.rs
@@ -0,0 +1,249 @@
+
+
+/// File-set ID
+/// 
+/// - **Tag:** (0004,1130)
+/// - **VR:** CS
+/// - **VM:** 1
+pub static FileSetID: Tag = Tag {
+    ident: "FileSetID",
+    tag: "(0004,1130)",
+    implicit_vr: Some(&vr::CS),
+    vm: "1",
+    desc: "File-set ID",
+};
+
+/// File-set Descriptor File ID
+/// 
+/// - **Tag:** (0004,1141)
+/// - **VR:** CS
+/// - **VM:** 1-8
+pub static FileSetDescriptorFileID: Tag = Tag {
+    ident: "FileSetDescriptorFileID",
+    tag: "(0004,1141)",
+    implicit_vr: Some(&vr::CS),
+    vm: "1-8",
+    desc: "File-set Descriptor File ID",
+};
+
+/// Specific Character Set of File-set Descriptor File
+/// 
+/// - **Tag:** (0004,1142)
+/// - **VR:** CS
+/// - **VM:** 1
+pub static SpecificCharacterSetOfFileSetDescriptorFile: Tag = Tag {
+    ident: "SpecificCharacterSetOfFileSetDescriptorFile",
+    tag: "(0004,1142)",
+    implicit_vr: Some(&vr::CS),
+    vm: "1",
+    desc: "Specific Character Set of File-set Descriptor File",
+};
+
+/// Offset of the First Directory Record of the Root Directory Entity
+/// 
+/// - **Tag:** (0004,1200)
+/// - **VR:** UL
+/// - **VM:** 1
+pub static OffsetOfTheFirstDirectoryRecordOfTheRootDirectoryEntity: Tag = Tag {
+    ident: "OffsetOfTheFirstDirectoryRecordOfTheRootDirectoryEntity",
+    tag: "(0004,1200)",
+    implicit_vr: Some(&vr::UL),
+    vm: "1",
+    desc: "Offset of the First Directory Record of the Root Directory Entity",
+};
+
+/// Offset of the Last Directory Record of the Root Directory Entity
+/// 
+/// - **Tag:** (0004,1202)
+/// - **VR:** UL
+/// - **VM:** 1
+pub static OffsetOfTheLastDirectoryRecordOfTheRootDirectoryEntity: Tag = Tag {
+    ident: "OffsetOfTheLastDirectoryRecordOfTheRootDirectoryEntity",
+    tag: "(0004,1202)",
+    implicit_vr: Some(&vr::UL),
+    vm: "1",
+    desc: "Offset of the Last Directory Record of the Root Directory Entity",
+};
+
+/// File-set Consistency Flag
+/// 
+/// - **Tag:** (0004,1212)
+/// - **VR:** US
+/// - **VM:** 1
+pub static FileSetConsistencyFlag: Tag = Tag {
+    ident: "FileSetConsistencyFlag",
+    tag: "(0004,1212)",
+    implicit_vr: Some(&vr::US),
+    vm: "1",
+    desc: "File-set Consistency Flag",
+};
+
+/// Directory Record Sequence
+/// 
+/// - **Tag:** (0004,1220)
+/// - **VR:** SQ
+/// - **VM:** 1
+pub static DirectoryRecordSequence: Tag = Tag {
+    ident: "DirectoryRecordSequence",
+    tag: "(0004,1220)",
+    implicit_vr: Some(&vr::SQ),
+    vm: "1",
+    desc: "Directory Record Sequence",
+};
+
+/// Offset of the Next Directory Record
+/// 
+/// - **Tag:** (0004,1400)
+/// - **VR:** UL
+/// - **VM:** 1
+pub static OffsetOfTheNextDirectoryRecord: Tag = Tag {
+    ident: "OffsetOfTheNextDirectoryRecord",
+    tag: "(0004,1400)",
+    implicit_vr: Some(&vr::UL),
+    vm: "1",
+    desc: "Offset of the Next Directory Record",
+};
+
+/// Record In-use Flag
+/// 
+/// - **Tag:** (0004,1410)
+/// - **VR:** US
+/// - **VM:** 1
+pub static RecordInUseFlag: Tag = Tag {
+    ident: "RecordInUseFlag",
+    tag: "(0004,1410)",
+    implicit_vr: Some(&vr::US),
+    vm: "1",
+    desc: "Record In-use Flag",
+};
+
+/// Offset of Referenced Lower-Level Directory Entity
+/// 
+/// - **Tag:** (0004,1420)
+/// - **VR:** UL
+/// - **VM:** 1
+pub static OffsetOfReferencedLowerLevelDirectoryEntity: Tag = Tag {
+    ident: "OffsetOfReferencedLowerLevelDirectoryEntity",
+    tag: "(0004,1420)",
+    implicit_vr: Some(&vr::UL),
+    vm: "1",
+    desc: "Offset of Referenced Lower-Level Directory Entity",
+};
+
+/// Directory Record Type
+/// 
+/// - **Tag:** (0004,1430)
+/// - **VR:** CS
+/// - **VM:** 1
+pub static DirectoryRecordType: Tag = Tag {
+    ident: "DirectoryRecordType",
+    tag: "(0004,1430)",
+    implicit_vr: Some(&vr::CS),
+    vm: "1",
+    desc: "Directory Record Type",
+};
+
+/// Private Record UID
+/// 
+/// - **Tag:** (0004,1432)
+/// - **VR:** UI
+/// - **VM:** 1
+pub static PrivateRecordUID: Tag = Tag {
+    ident: "PrivateRecordUID",
+    tag: "(0004,1432)",
+    implicit_vr: Some(&vr::UI),
+    vm: "1",
+    desc: "Private Record UID",
+};
+
+/// Referenced File ID
+/// 
+/// - **Tag:** (0004,1500)
+/// - **VR:** CS
+/// - **VM:** 1-8
+pub static ReferencedFileID: Tag = Tag {
+    ident: "ReferencedFileID",
+    tag: "(0004,1500)",
+    implicit_vr: Some(&vr::CS),
+    vm: "1-8",
+    desc: "Referenced File ID",
+};
+
+/// MRDR Directory Record Offset
+/// 
+/// - **Tag:** (0004,1504)
+/// - **VR:** UL
+/// - **VM:** 1
+pub static MRDRDirectoryRecordOffset: Tag = Tag {
+    ident: "MRDRDirectoryRecordOffset",
+    tag: "(0004,1504)",
+    implicit_vr: Some(&vr::UL),
+    vm: "1",
+    desc: "MRDR Directory Record Offset",
+};
+
+/// Referenced SOP Class UID in File
+/// 
+/// - **Tag:** (0004,1510)
+/// - **VR:** UI
+/// - **VM:** 1
+pub static ReferencedSOPClassUIDInFile: Tag = Tag {
+    ident: "ReferencedSOPClassUIDInFile",
+    tag: "(0004,1510)",
+    implicit_vr: Some(&vr::UI),
+    vm: "1",
+    desc: "Referenced SOP Class UID in File",
+};
+
+/// Referenced SOP Instance UID in File
+/// 
+/// - **Tag:** (0004,1511)
+/// - **VR:** UI
+/// - **VM:** 1
+pub static ReferencedSOPInstanceUIDInFile: Tag = Tag {
+    ident: "ReferencedSOPInstanceUIDInFile",
+    tag: "(0004,1511)",
+    implicit_vr: Some(&vr::UI),
+    vm: "1",
+    desc: "Referenced SOP Instance UID in File",
+};
+
+/// Referenced Transfer Syntax UID in File
+/// 
+/// - **Tag:** (0004,1512)
+/// - **VR:** UI
+/// - **VM:** 1
+pub static ReferencedTransferSyntaxUIDInFile: Tag = Tag {
+    ident: "ReferencedTransferSyntaxUIDInFile",
+    tag: "(0004,1512)",
+    implicit_vr: Some(&vr::UI),
+    vm: "1",
+    desc: "Referenced Transfer Syntax UID in File",
+};
+
+/// Referenced Related General SOP Class UID in File
+/// 
+/// - **Tag:** (0004,151A)
+/// - **VR:** UI
+/// - **VM:** 1-n
+pub static ReferencedRelatedGeneralSOPClassUIDInFile: Tag = Tag {
+    ident: "ReferencedRelatedGeneralSOPClassUIDInFile",
+    tag: "(0004,151A)",
+    implicit_vr: Some(&vr::UI),
+    vm: "1-n",
+    desc: "Referenced Related General SOP Class UID in File",
+};
+
+/// Number of References
+/// 
+/// - **Tag:** (0004,1600)
+/// - **VR:** UL
+/// - **VM:** 1
+pub static NumberOfReferences: Tag = Tag {
+    ident: "NumberOfReferences",
+    tag: "(0004,1600)",
+    implicit_vr: Some(&vr::UL),
+    vm: "1",
+    desc: "Number of References",
+};
+
