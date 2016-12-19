45269340c6284cf6addfc233461ec05e4ef72128
Christopher Speck <die.drachen@gmail.com>
2016-12-19 00:32:01 -0500

    Switching to using perfect hash function for lookups. Still some kinks to work out

--- src/core/dict/mod.rs
+++ src/core/dict/mod.rs
@@ -1,8 +1,6 @@
 pub mod dicom_elements;
 pub mod dir_structure_elements;
 pub mod file_meta_elements;
-pub mod tag_lookup;
+pub mod lookup;
 pub mod transfer_syntaxes;
-pub mod ts_lookup;
-pub mod uid_lookup;
 pub mod uids;
