7bf66aa3e622d96942a2957f8f3930619ff34a53
Christopher Speck <die.drachen@gmail.com>
2018-11-07 22:29:47 -0500

    T3 - Comment clean up

--- lib/build/build.rs
+++ lib/build/build.rs
@@ -9,11 +9,7 @@ use std::path::Path;
 static DICOM_CONSTANTS_HTML_FILE: &'static str = "build/dicom_xml/part06.xml";
 
 fn main() {
-    /// In theory this instructs cargo that this build script doesn't need run
-    /// unless the file `DICOM_CONSTANTS_HTML_FILE` has changed.
-    /// This does appear to work, however cargo is also "smart" and will ensure
-    /// this build script runs fresh if this script file has changed even if the
-    /// file hasn't.
+    // Causes this build script to re-run if the file is modified/changed
     println!("cargo:rerun-if-changed=./{}", DICOM_CONSTANTS_HTML_FILE);
 
     let file: File = File::open(DICOM_CONSTANTS_HTML_FILE)
