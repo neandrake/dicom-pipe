c66a433408dcd3b7205380ab3d98b3cb0455f495
Christopher Speck <die.drachen@gmail.com>
2019-08-24 23:16:42 -0400

    T19: Remove tui version for now

--- dcmpipe_cli/src/main.rs
+++ dcmpipe_cli/src/main.rs
@@ -3,16 +3,15 @@ extern crate cursive;
 extern crate cursive_table_view;
 extern crate dcmpipe_dict;
 extern crate dcmpipe_lib;
-extern crate tui;
 
 mod app;
 
 use crate::app::CursiveApp;
-use app::{FullObjApp, LowMemApp, TuiApp};
+use app::{FullObjApp, LowMemApp};
 use std::io::{Error, ErrorKind};
 use std::{env, process};
 
-static APP_MODE: usize = 1;
+static APP_MODE: usize = 3;
 
 fn main() {
     if let Err(e) = runapp() {
@@ -35,8 +34,7 @@ fn runapp() -> Result<(), Error> {
     match APP_MODE {
         1 => LowMemApp::new(openpath).run(),
         2 => FullObjApp::new(openpath).run(),
-        3 => TuiApp::new(openpath).run(),
-        4 => CursiveApp::new(openpath).run(),
+        3 => CursiveApp::new(openpath).run(),
         _ => Err(Error::new(ErrorKind::InvalidInput, "Invalid app mode")),
     }
 }
