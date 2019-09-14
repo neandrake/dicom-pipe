5ac30d275a3564a326d32715103e4567c51de566
Christopher Speck <die.drachen@gmail.com>
2019-09-14 16:41:41 -0400

    maint: rename `stream` to `dataset`

--- dcmpipe_lib/src/core/tagstop.rs
+++ dcmpipe_lib/src/core/tagstop.rs
@@ -1,16 +1,15 @@
-/// TagStop specifies the stopping point at which parsing of a DICOM stream
-/// should end.
+/// TagStop specifies the stopping point at which parsing of a DICOM dataset should end.
 pub enum TagStop {
-    /// The entire stream should be parsed.
-    EndOfStream,
-    /// Read all tag elements up to (but not including) the specified tag.
-    /// The tag value is interpreted only at the root of an object and not within sequences.
+    /// The entire dataset should be parsed.
+    EndOfDataset,
+    /// Read all tag elements up to (but not including) the specified tag. The tag value is
+    /// interpreted only at the root of an object and not within sequences.
     BeforeTag(u32),
-    /// Read all tag elements up to (and including) the specified tag.
-    /// The tag value is interpreted only at the root of an object and not within sequences.
+    /// Read all tag elements up to (and including) the specified tag. The tag value is interpreted
+    /// only at the root of an object and not within sequences.
     AfterTag(u32),
-    /// Read all tag elements up to specified number of bytes have been read.
-    /// If the byte position is in the middle of an element then bytes from
-    /// that stream will continue to be read until the elment is fully parsed.
+    /// Read all tag elements up to specified number of bytes have been read. If the byte position
+    /// is in the middle of an element then bytes from that dataset will continue to be read until
+    /// the elment is fully parsed.
     AfterBytePos(u64),
 }
