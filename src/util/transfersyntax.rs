dd9a89ffc23cfcefcb3c9b60fcc68df0c2064018
Christopher Speck <cspeckrun@gmail.com>
2024-04-28 22:31:31 -0400

    Added lifetime specifier to transfer syntax, changing static UID/TS to const UID/TS

--- src/util/transfersyntax.rs
+++ src/util/transfersyntax.rs
@@ -5,16 +5,17 @@ use std::hash::{Hash, Hasher};
 
 use util::uids::UID;
 
-pub struct TransferSyntax {
-    uid: &'static UID,
+#[derive(Debug)]
+pub struct TransferSyntax<'uid_lifetime> {
+    uid: &'uid_lifetime UID,
     big_endian: bool,
     explicit_vr: bool,
     deflated: bool,
     encapsulated: bool,
 }
 
-impl TransferSyntax {
-    pub fn new(uid: &'static UID,
+impl<'uid_lifetime> TransferSyntax<'uid_lifetime> {
+    pub fn new<'new_uid_lifetime>(uid: &'new_uid_lifetime UID,
         big_endian: bool,
         explicit_vr: bool,
         deflated: bool,
@@ -28,7 +29,7 @@ impl TransferSyntax {
         }
     }
 
-    pub fn get_uid(&self) -> &'static UID {
+    pub fn get_uid(&self) -> &'uid_lifetime UID {
         self.uid
     }
 
@@ -53,23 +54,40 @@ impl TransferSyntax {
     }
 }
 
-impl PartialEq for TransferSyntax {
+impl<'uid_lifetime> PartialEq for TransferSyntax<'uid_lifetime> {
     fn eq(&self, other: &TransferSyntax) -> bool {
         self.uid.eq(other.uid)
     }
 }
 
-impl Hash for TransferSyntax {
+impl<'uid_lifetime> Hash for TransferSyntax<'uid_lifetime> {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.uid.hash(state);
     }
 }
 
 
-pub static ImplicitVRLittleEndian: &'static TransferSyntax = &TransferSyntax {
+pub const ImplicitVRLittleEndian: TransferSyntax<'static> = TransferSyntax {
     uid: & ::util::uids::ImplicitVRLittleEndian,
     big_endian: false,
     explicit_vr: false,
     deflated: false,
     encapsulated: false,
 };
+
+#[test]
+fn test_diff_ts_instances_eq() {
+    let ts_uid: UID = UID::new(
+        "1.2.840.10008.1.2",
+        "Blarf ImplicitVRLittleEndian",
+        "Blarf Implicit VR Little Endian",
+    );
+    let implicit_vr_le: TransferSyntax = TransferSyntax::new(
+        &ts_uid,
+        false,
+        false,
+        false,
+        false,
+    );
+    assert_eq!(ImplicitVRLittleEndian, implicit_vr_le);
+}
