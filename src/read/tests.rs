19ac02e5fcda63e11c9ee3c6fa331357d7c2958a
Christopher Speck <cspeckrun@gmail.com>
2024-04-28 22:31:34 -0400

    Updates to parsing of FileMetaInformation
    Summary:
    1. Added `tag_peek` so the next tag to read can be peek'd without parsing through the entire element
    2. Updated `read_file_meta()` to use the peek'd tag in order to properly handle when to stop parsing elements

--- src/read/tests.rs
+++ src/read/tests.rs
@@ -1,11 +1,16 @@
 #![cfg(test)]
 
+extern crate byteorder;
 extern crate walkdir;
 
+use byteorder::ReadBytesExt;
+
+use read::{DICOM_PREFIX, DICOM_PREFIX_LENGTH, FILE_PREAMBLE_LENGTH};
 use read::DicomStream;
 use read::mock::MockDicomStream;
 
 use std::fs::File;
+use std::io::Seek;
 use std::path::Path;
 
 use walkdir::{DirEntry, WalkDir};
@@ -16,9 +21,11 @@ static FIXTURE_DATASET1_FOLDER: &'static str = "fixtures/dataset1";
 fn test_good_preamble() {
     let mut test_good_stream: DicomStream<MockDicomStream> =
         MockDicomStream::standard_dicom_preamble();
-    test_good_stream.read_file_preamble().expect("Unable to read file preamble");
-    test_good_stream.read_dicom_prefix().expect("Unable to read DICOM prefix");
-    let is_dcm: bool = test_good_stream.is_standard_preamble();
+    test_good_stream.read_file_preamble()
+        .expect("Unable to read file preamble");
+    test_good_stream.read_dicom_prefix()
+        .expect("Unable to read DICOM prefix");
+    let is_dcm: bool = is_standard_preamble(&test_good_stream);
     assert_eq!(is_dcm, true);
 }
 
@@ -26,9 +33,11 @@ fn test_good_preamble() {
 fn test_nonzero_preamble() {
     let mut test_size_stream: DicomStream<MockDicomStream> =
         MockDicomStream::nonzero_preamble();
-    test_size_stream.read_file_preamble().expect("Unable to read file preamble");
-    test_size_stream.read_dicom_prefix().expect("Unable to read DICOM prefix");
-    let is_dcm: bool = test_size_stream.is_standard_preamble();
+    test_size_stream.read_file_preamble()
+        .expect("Unable to read file preamble");
+    test_size_stream.read_dicom_prefix()
+        .expect("Unable to read DICOM prefix");
+    let is_dcm: bool = is_standard_preamble(&test_size_stream);
     assert_eq!(is_dcm, false);
 }
 
@@ -37,9 +46,11 @@ fn test_nonzero_preamble() {
 fn test_bad_dicom_prefix() {
     let mut test_bad_stream: DicomStream<MockDicomStream> =
         MockDicomStream::invalid_dicom_prefix();
-    test_bad_stream.read_file_preamble().expect("Unable to read file preamble");
-    test_bad_stream.read_dicom_prefix().expect("This should fail to read a valid prefix");
-    let is_dcm: bool = test_bad_stream.is_standard_preamble();
+    test_bad_stream.read_file_preamble()
+        .expect("Unable to read file preamble");
+    test_bad_stream.read_dicom_prefix()
+        .expect("This should fail to read a valid prefix");
+    let is_dcm: bool = is_standard_preamble(&test_bad_stream);
     assert_eq!(is_dcm, false);
 }
 
@@ -48,7 +59,8 @@ fn test_bad_dicom_prefix() {
 fn test_failure_to_read_preamble() {
     let mut test_stream: DicomStream<MockDicomStream> =
         MockDicomStream::standard_dicom_preamble_diff_startpos_and_short_stream();
-    test_stream.read_file_preamble().expect("This should fail to read preamble due to not enough data");
+    test_stream.read_file_preamble()
+        .expect("This should fail to read preamble due to not enough data");
     let start_pos: usize = test_stream.stream.pos;
     assert!(start_pos != 0);
 }
@@ -64,8 +76,25 @@ fn test_parse_known_dicom_files() {
         let path: &Path = entry.path();
         let mut dstream: DicomStream<File> = DicomStream::new_from_path(path)
             .expect("Unable to read file");
-        dstream.read_file_meta().expect("Unable to read FileMetaInformation");
-        let is_dcm = dstream.is_standard_preamble();
-        assert_eq!(is_dcm, true);
+        dstream.read_file_meta()
+            .expect(&format!("Unable to read FileMetaInformation: {:?}", path));
+        let is_dcm = is_standard_preamble(&dstream);
+        assert!(is_dcm);
     }
+}
+
+/// Checks that the first 132 bytes are 128 0's followed by 'DICM'
+fn is_standard_preamble<StreamType>(stream: &DicomStream<StreamType>) -> bool
+    where StreamType: ReadBytesExt + Seek {
+    for i in 0..FILE_PREAMBLE_LENGTH {
+        if stream.file_preamble[i] != 0 {
+            return false;
+        }
+    }
+    for i in 0..DICOM_PREFIX_LENGTH {
+        if stream.dicom_prefix[i] != DICOM_PREFIX[i] {
+            return false;
+        }
+    }
+    true
 }
