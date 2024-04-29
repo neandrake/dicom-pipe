475036ce391139f205f5ae0271f838b738bc3c40
Christopher Speck <cspeckrun@gmail.com>
2024-04-28 22:31:30 -0400

    Added implementations for TestDicomStream

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ fn parse_directory(dirwalker: WalkDir) -> Result<(), Error> {
 		let entry: DirEntry = try!(entry_res);
 		let dstream_res: Result<Box<DicomStream>, Error> = open_file_as_dicom_stream(&entry);
 		match dstream_res {
-			Ok(val) => {
+			Ok(_) => {
 				println!("[DEBUG] File is DICOM: {:?}", entry.path());
 			},
 			Err(err) => {
@@ -106,16 +106,18 @@ fn open_file_as_dicom_stream(entry: &DirEntry) -> Result<Box<DicomStream>, Error
 
 // --- Test
 
-struct TestDicomHeader {
+#[cfg(test)]
+struct TestDicomStream {
 	data: Vec<u8>,
 	pos: usize,
 }
 
-impl TestDicomHeader {
-	pub fn valid_dicom_preamble() -> TestDicomHeader {
-		TestDicomHeader {
+#[cfg(test)]
+impl TestDicomStream {
+	pub fn standard_dicom_preamble() -> TestDicomStream {
+		TestDicomStream {
 			data : {
-				let mut data = vec![0u8;132];
+				let mut data: Vec<u8> = vec![0u8;132];
 				data[128] = 'D' as u8;
 				data[129] = 'I' as u8;
 				data[130] = 'C' as u8;
@@ -127,30 +129,57 @@ impl TestDicomHeader {
 	}
 }
 
-impl Read for TestDicomHeader {
+#[cfg(test)]
+impl Read for TestDicomStream {
 	fn read(&mut self, buf: &mut [u8]) -> Result<usize, Error> {
+		if self.pos >= self.data.len() {
+			return Result::Ok(0);
+		}
 		
-		Result::Ok(0)
+		let mut count: usize = 0;
+		for i in self.pos..self.data.len() {
+			if count >= buf.len() {
+				break;
+			}
+			buf[count] = self.data[i];
+			count += 1;
+		}
+		self.pos = self.pos + count;
+		Result::Ok(count)
 	}
 }
 
-impl Seek for TestDicomHeader {
+#[cfg(test)]
+impl Seek for TestDicomStream {
 	fn seek(&mut self, pos: SeekFrom) -> Result<u64, Error> {
-		Result::Ok(0)
+		let newpos: usize = match pos {
+			SeekFrom::Start(n) => 0usize.saturating_add(n as usize),
+			SeekFrom::Current(n) => self.pos.saturating_add(n as usize),
+			SeekFrom::End(n) => self.data.len().saturating_sub(n as usize),
+		};
+		
+		if newpos < self.data.len() {
+			self.pos = newpos;
+			return Result::Ok(newpos as u64);
+		}
+		
+		return Result::Err(Error::new(
+			ErrorKind::UnexpectedEof,
+			format!("seek to invalid position: {:?}", newpos)));
 	}
 }
 
-impl DicomStream for TestDicomHeader {
-	
-}
+#[cfg(test)]
+impl DicomStream for TestDicomStream {}
 
 #[test]
 fn test_preamble() {
-	
+	let mut test_stream = TestDicomStream::standard_dicom_preamble();
+	test_stream.is_standard_dicom().expect("should be valid dicom");
 }
 
 #[test]
-fn test_parse_known_dicom() {
+fn test_parse_known_dicom_files() {
 	let dirwalker: WalkDir = WalkDir::new(FIXTURE_DATASET1_FOLDER)
 										.min_depth(1)
 										.max_depth(1);
