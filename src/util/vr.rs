9bc1b813c0b53b8b25c75085a71303ee41eb44c9
Christopher Speck <cspeckrun@gmail.com>
2024-04-28 22:31:32 -0400

    Updates to reading -- DICOM File Meta Information is being read appropriately (required fields only)

--- src/util/vr.rs
+++ src/util/vr.rs
@@ -3,6 +3,8 @@
 
 
 pub struct VR {
+	pub ident: &'static str,
+	pub name: &'static str,
 	pub code: u32,
 	pub padding: u32,
 	pub explicit_vr_header_length: u32,
@@ -45,6 +47,8 @@ impl VR {
 
 /// Application Entity (<= 16 chars/u16)
 pub static AE: &'static VR = &VR {
+	ident: "AE",
+	name: "Application Entity",
 	code: 0x4145,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
@@ -52,6 +56,8 @@ pub static AE: &'static VR = &VR {
 
 /// Age String
 pub static AS: &'static VR = &VR {
+	ident: "AS",
+	name: "Age String",
 	code: 0x4153,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
@@ -59,6 +65,8 @@ pub static AS: &'static VR = &VR {
 
 /// Attribute Tag
 pub static AT: &'static VR = &VR {
+	ident: "AT",
+	name: "Attribute Tag",
 	code: 0x4154,
 	padding: 0x0,
 	explicit_vr_header_length: 8,
@@ -66,6 +74,8 @@ pub static AT: &'static VR = &VR {
 
 /// Code String (<= 16 chars/u16)
 pub static CS: &'static VR = &VR {
+	ident: "CS",
+	name: "Code String",
 	code: 0x4353,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
@@ -73,6 +83,8 @@ pub static CS: &'static VR = &VR {
 
 /// Date
 pub static DA: &'static VR = &VR {
+	ident: "DA",
+	name: "Date",
 	code: 0x4441,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
@@ -80,6 +92,8 @@ pub static DA: &'static VR = &VR {
 
 /// Decimal String (=> f32)
 pub static DS: &'static VR = &VR {
+	ident: "DS",
+	name: "Decimal String",
 	code: 0x4453,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
@@ -87,6 +101,8 @@ pub static DS: &'static VR = &VR {
 
 /// Date Time
 pub static DT: &'static VR = &VR {
+	ident: "DT",
+	name: "Date Time",
 	code: 0x4454,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
@@ -94,6 +110,8 @@ pub static DT: &'static VR = &VR {
 
 /// Floating Point Double (=> f64)
 pub static FD: &'static VR = &VR {
+	ident: "FD",
+	name: "Floating Point Double",
 	code: 0x4644,
 	padding: 0x0,
 	explicit_vr_header_length: 8,
@@ -101,6 +119,8 @@ pub static FD: &'static VR = &VR {
 
 /// Floating Point Single (=> f32)
 pub static FL: &'static VR = &VR {
+	ident: "FL",
+	name: "Floating Point Single",
 	code: 0x464C,
 	padding: 0x0,
 	explicit_vr_header_length: 8,
@@ -108,6 +128,8 @@ pub static FL: &'static VR = &VR {
 
 /// Integer String (=> i32)
 pub static IS: &'static VR = &VR {
+	ident: "IS",
+	name: "Integer String",
 	code: 0x4953,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
@@ -115,6 +137,8 @@ pub static IS: &'static VR = &VR {
 
 /// Long String (<= 64 chars/u16)
 pub static LO: &'static VR = &VR {
+	ident: "LO",
+	name: "Long String",
 	code: 0x4C4F,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
@@ -122,27 +146,35 @@ pub static LO: &'static VR = &VR {
 
 /// Long Text (<= 10240 chars/u16)
 pub static LT: &'static VR = &VR {
+	ident: "LT",
+	name: "Long Text",
 	code: 0x4C54,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
 };
 
-/// Other Byte String
+/// Other Byte
 pub static OB: &'static VR = &VR {
+	ident: "OB",
+	name: "Other Byte",
 	code: 0x4F42,
 	padding: 0x0,
 	explicit_vr_header_length: 12,
 };
 
-/// Other Float String
+/// Other Float
 pub static OF: &'static VR = &VR {
+	ident: "OF",
+	name: "Other Float",
 	code: 0x4F46,
 	padding: 0x0,
 	explicit_vr_header_length: 12,
 };
 
-/// Other Word String
+/// Other Word
 pub static OW: &'static VR = &VR {
+	ident: "OW",
+	name: "Other Float",
 	code: 0x4F57,
 	padding: 0x0,
 	explicit_vr_header_length: 12,
@@ -150,6 +182,8 @@ pub static OW: &'static VR = &VR {
 
 /// Person Name (component group <= 64 chars/u16)
 pub static PN: &'static VR = &VR {
+	ident: "PN",
+	name: "Person Name",
 	code: 0x504E,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
@@ -157,6 +191,8 @@ pub static PN: &'static VR = &VR {
 
 /// Short String (<= 16 chars/u16)
 pub static SH: &'static VR = &VR {
+	ident: "SH",
+	name: "Short String",
 	code: 0x5348,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
@@ -164,6 +200,8 @@ pub static SH: &'static VR = &VR {
 
 /// Signed Long (-2147483648..+2147483647)
 pub static SL: &'static VR = &VR {
+	ident: "SL",
+	name: "Signed Long",
 	code: 0x534C,
 	padding: 0x20,
 	explicit_vr_header_length: 8
@@ -171,6 +209,8 @@ pub static SL: &'static VR = &VR {
 
 /// Sequence of Items
 pub static SQ: &'static VR = &VR {
+	ident: "SQ",
+	name: "Sequence of Items",
 	code: 0x5351,
 	padding: 0x0,
 	explicit_vr_header_length: 12,
@@ -178,6 +218,8 @@ pub static SQ: &'static VR = &VR {
 
 /// Signed Short (-32768..+32767)
 pub static SS: &'static VR = &VR {
+	ident: "SS",
+	name: "Signed Short",
 	code: 0x5353,
 	padding: 0x0,
 	explicit_vr_header_length: 8,
@@ -185,6 +227,8 @@ pub static SS: &'static VR = &VR {
 
 /// Short Text (<= 1024 chars/u16)
 pub static ST: &'static VR = &VR {
+	ident: "ST",
+	name: "Short Text",
 	code: 0x5354,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
@@ -192,6 +236,8 @@ pub static ST: &'static VR = &VR {
 
 /// Time
 pub static TM: &'static VR = &VR {
+	ident: "TM",
+	name: "Time",
 	code: 0x544D,
 	padding: 0x20,
 	explicit_vr_header_length: 8,
@@ -199,6 +245,8 @@ pub static TM: &'static VR = &VR {
 
 /// Unique Identifier (UID) (<= 64 chars/u16)
 pub static UI: &'static VR = &VR {
+	ident: "UI",
+	name: "Unique Identifier (UID)",
 	code: 0x5549,
 	padding: 0x0,
 	explicit_vr_header_length: 8,
@@ -206,13 +254,17 @@ pub static UI: &'static VR = &VR {
 
 /// Unsigned Long (0..4294967295)
 pub static UL: &'static VR = &VR {
+	ident: "UL",
+	name: "Unsigned Long",
 	code: 0x554C,
 	padding: 0x0,
 	explicit_vr_header_length: 8,
 };
 
-/// Unkown
+/// Unknown
 pub static UN: &'static VR = &VR {
+	ident: "UN",
+	name: "Unknown",
 	code: 0x544E,
 	padding: 0x0,
 	explicit_vr_header_length: 12,
@@ -220,6 +272,8 @@ pub static UN: &'static VR = &VR {
 
 /// Unsigned Short (0..65535)
 pub static US: &'static VR = &VR {
+	ident: "US",
+	name: "Unsigned Short",
 	code: 0x5553,
 	padding: 0,
 	explicit_vr_header_length: 8,
@@ -227,6 +281,8 @@ pub static US: &'static VR = &VR {
 
 /// Unlimited Text (<= 4294967294 chars/u16)
 pub static UT: &'static VR = &VR {
+	ident: "UT",
+	name: "Unlimited Text",
 	code: 0x5554,
 	padding: 0x20,
 	explicit_vr_header_length: 12,
