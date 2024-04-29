fe67b4288d02f3c63d7c12759fa503f67265757b
Christopher Speck <cspeckrun@gmail.com>
2024-04-28 22:32:02 -0400

    Add a wrapper to encapsulate multiple dictionaries for use when parsing

--- dcmpipe_lib/src/defn/dcmdict.rs
+++ dcmpipe_lib/src/defn/dcmdict.rs
@@ -20,3 +20,43 @@ pub trait DicomDictionary {
     /// Look up a `UID` definition by its UID
     fn get_uid_by_uid(&self, uid: &str) -> Option<UIDRef>;
 }
+
+/// Wraps a list of DICOM dictionaries to be used a single dictionary. The order of the
+/// dictionaries affects the resulting lookup if the search key exists in multiple dictionaries.
+pub struct MultiDicomDictionary<'dict> {
+    dicts: Vec<&'dict dyn DicomDictionary>,
+}
+
+impl <'dict> MultiDicomDictionary<'dict> {
+    pub fn new(dicts: Vec<&'dict dyn DicomDictionary>) -> Self {
+        Self {
+            dicts
+        }
+    }
+}
+
+impl <'dict> DicomDictionary for MultiDicomDictionary<'dict> {
+    fn get_ts_by_name(&self, name: &str) -> Option<TSRef> {
+        self.dicts.iter().flat_map(|d| d.get_ts_by_name(name)).nth(0)
+    }
+
+    fn get_ts_by_uid(&self, uid: &str) -> Option<TSRef> {
+        self.dicts.iter().flat_map(|d| d.get_ts_by_uid(uid)).nth(0)
+    }
+
+    fn get_tag_by_name(&self, name: &str) -> Option<TagRef> {
+        self.dicts.iter().flat_map(|d| d.get_tag_by_name(name)).nth(0)
+    }
+
+    fn get_tag_by_number(&self, number: u32) -> Option<TagRef> {
+        self.dicts.iter().flat_map(|d| d.get_tag_by_number(number)).nth(0)
+    }
+
+    fn get_uid_by_name(&self, name: &str) -> Option<UIDRef> {
+        self.dicts.iter().flat_map(|d| d.get_uid_by_name(name)).nth(0)
+    }
+
+    fn get_uid_by_uid(&self, uid: &str) -> Option<UIDRef> {
+        self.dicts.iter().flat_map(|d| d.get_uid_by_uid(uid)).nth(0)
+    }
+}
