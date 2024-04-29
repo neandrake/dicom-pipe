ea265234c58f132d2c979d2463b1e35f30cb8a4d
Christopher Speck <cspeckrun@gmail.com>
2024-04-28 22:44:04 -0400

    chore: change order of assertion arguments to 'expected, actual'

--- dcmpipe_lib/src/tests/tests.rs
+++ dcmpipe_lib/src/tests/tests.rs
@@ -13,26 +13,26 @@ use crate::dict::uids;
 pub fn test_tags_lookup() {
     // Lookup a DICOM Element by identity/value from the TAG maps
     let pd_by_ident: &Tag = TAG_BY_IDENT.get("PixelData").expect("Tag not found");
-    assert_eq!(pd_by_ident, &tags::PixelData);
+    assert_eq!(&tags::PixelData, pd_by_ident);
 
     let pd_by_tag: &Tag = TAG_BY_VALUE.get(&0x7FE00010).expect("Tag not found");
-    assert_eq!(pd_by_tag, &tags::PixelData);
+    assert_eq!(&tags::PixelData, pd_by_tag);
 
     // Lookup a Directory Structure Element by identity/value from the TAG maps
     let fsid_by_ident: &Tag = TAG_BY_IDENT.get("FilesetID").expect("Tag not found");
-    assert_eq!(fsid_by_ident, &tags::FilesetID);
+    assert_eq!(&tags::FilesetID, fsid_by_ident);
 
     let fsid_by_tag: &Tag = TAG_BY_VALUE.get(&0x00041130).expect("Tag not found");
-    assert_eq!(fsid_by_tag, &tags::FilesetID);
+    assert_eq!(&tags::FilesetID, fsid_by_tag);
 
     // Lookup a File Meta Element by identity/value from the TAG maps
     let tsuid_by_ident: &Tag = TAG_BY_IDENT
         .get("TransferSyntaxUID")
         .expect("Tag not found");
-    assert_eq!(tsuid_by_ident, &tags::TransferSyntaxUID);
+    assert_eq!(&tags::TransferSyntaxUID, tsuid_by_ident);
 
     let tsuid_by_tag: &Tag = TAG_BY_VALUE.get(&0x00020010).expect("Tag not found");
-    assert_eq!(tsuid_by_tag, &tags::TransferSyntaxUID);
+    assert_eq!(&tags::TransferSyntaxUID, tsuid_by_tag);
 }
 
 #[test]
@@ -40,43 +40,43 @@ pub fn test_transfer_syntaxes_lookup() {
     let ivrle_by_ident: &TransferSyntax = TS_BY_IDENT
         .get("ImplicitVRLittleEndian")
         .expect("TransferSyntax not found");
-    assert_eq!(ivrle_by_ident, &ts::ImplicitVRLittleEndian);
+    assert_eq!(&ts::ImplicitVRLittleEndian, ivrle_by_ident);
 
     let ivrle_by_id: &TransferSyntax = TS_BY_UID
         .get("1.2.840.10008.1.2")
         .expect("TransferSyntax not found");
-    assert_eq!(ivrle_by_id, &ts::ImplicitVRLittleEndian);
+    assert_eq!(&ts::ImplicitVRLittleEndian, ivrle_by_id);
 
     let ivrle_by_uid: &TransferSyntax = TS_BY_UID
         .get(&uids::ImplicitVRLittleEndian.uid)
         .expect("TransferSyntax not found");
-    assert_eq!(ivrle_by_uid, &ts::ImplicitVRLittleEndian);
+    assert_eq!(&ts::ImplicitVRLittleEndian, ivrle_by_uid);
 
     let evrle_by_ident: &TransferSyntax = TS_BY_IDENT
         .get("ExplicitVRLittleEndian")
         .expect("TransferSyntax not found");
-    assert_eq!(evrle_by_ident, &ts::ExplicitVRLittleEndian);
+    assert_eq!(&ts::ExplicitVRLittleEndian, evrle_by_ident);
 
     let evrle_by_id: &TransferSyntax = TS_BY_UID
         .get("1.2.840.10008.1.2.1")
         .expect("TransferSyntax not found");
-    assert_eq!(evrle_by_id, &ts::ExplicitVRLittleEndian);
+    assert_eq!(&ts::ExplicitVRLittleEndian, evrle_by_id);
 
     let evrle_by_uid: &TransferSyntax = TS_BY_UID
         .get(&uids::ExplicitVRLittleEndian.uid)
         .expect("TransferSyntax not found");
-    assert_eq!(evrle_by_uid, &ts::ExplicitVRLittleEndian);
+    assert_eq!(&ts::ExplicitVRLittleEndian, evrle_by_uid);
 }
 
 #[test]
 pub fn test_uids_lookup() {
     let ctis_by_ident: UIDRef = UID_BY_IDENT.get("CTImageStorage").expect("UID not found");
-    assert_eq!(ctis_by_ident, &uids::CTImageStorage);
+    assert_eq!(&uids::CTImageStorage, ctis_by_ident);
 
     let ctis_by_id: UIDRef = UID_BY_UID
         .get("1.2.840.10008.5.1.4.1.1.2")
         .expect("UID not found");
-    assert_eq!(ctis_by_id, &uids::CTImageStorage);
+    assert_eq!(&uids::CTImageStorage, ctis_by_id);
 }
 
 /// Sanity-check of the pre-defined TransferSyntax's to ensure
